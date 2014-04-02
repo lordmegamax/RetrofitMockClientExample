24589636840b6e4a9cdcd646d9e72ea944007f3c
Max Melnychuk <lordmegamax@gmail.com>
2014-04-02 23:11:58 +0300

    Create constant with website url.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,12 @@ package com.company;
 import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
-import retrofit.client.Client;
-import retrofit.client.Request;
 import retrofit.client.Response;
-import retrofit.converter.ConversionException;
-import retrofit.converter.Converter;
 import retrofit.http.GET;
-import retrofit.mime.TypedByteArray;
-import retrofit.mime.TypedInput;
-import retrofit.mime.TypedOutput;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Type;
-import java.net.URI;
-import java.util.Collections;
 
 public class Main {
+    public static final String WEBSITE_URL = "http://www.example.com";
+
     public interface ApiService {
         @GET("/api/")
         public void getJson(Callback<String> callback);
@@ -31,7 +18,7 @@ public class Main {
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setClient(new MockClient())
                 .setConverter(new StringConverter())
-                .setEndpoint("http://www.example.com").build();
+                .setEndpoint(WEBSITE_URL).build();
 
         ApiService service = restAdapter.create(ApiService.class);
         service.getJson(new Callback<String>() {
